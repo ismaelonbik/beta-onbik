f8fa1807997a667b02a184d0097728d44f77ef5f
Ismael Castillo <ismaelcastillo@onbik.com>
2019-01-31 11:01:56 +0100

    prueba button

--- src/service-worker.js
+++ src/service-worker.js
@@ -15,27 +15,4 @@ self.addEventListener('fetch', function(event) {
       )
   );
 });
-document.getElementById("instalButton").addEventListener('click', function() {
-  console.log('ha pulsado el boton');
-  if(deferredPrompt !== undefined) {
-    // The user has had a postive interaction with our app and Chrome
-    // has tried to prompt previously, so let's show the prompt.
-    deferredPrompt.prompt();
 
-    // Follow what the user has done with the prompt.
-    deferredPrompt.userChoice.then(function(choiceResult) {
-
-      console.log(choiceResult.outcome);
-
-      if(choiceResult.outcome == 'dismissed') {
-        console.log('User cancelled home screen install');
-      }
-      else {
-        console.log('User added to home screen');
-      }
-
-      // We no longer need the prompt.  Clear it up.
-      deferredPrompt = null;
-    });
-  }
-});
